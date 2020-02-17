4f6128b6c419406e33f224d148bfa6c827aa788b
Muhammad Syed <msyed92@outlook.com>
2020-02-17 11:13:29 -0500

    asdfsd

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        backgroundColor: Colors.blue,
-        body: Center(
-          child: Image(
-              image: NetworkImage(
-                  'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I Am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Image(
+          image: AssetImage('images/speedwagon.png'),
         ),
       ),
     ),
-  );
+  ));
 }
